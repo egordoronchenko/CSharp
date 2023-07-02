600c15925caba8dee0cde277f47179082cae8e17
Egor Doronchenko <massda@yandex.ru>
2023-07-02 20:01:17 +0300

    Добавлена задачка про Машу

---
+++ Examples/Examples005_IfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите имя пользователя");
+string userName = Console.ReadLine();
+
+if(userName.ToLower == "Маша")
+{
+    Console.WriteLine("Ура! Это Маша");
+}
+else
+{
+Console.WriteLine("Привет, "+ userName);
+
+}
