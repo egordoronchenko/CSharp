0f9bc6fe2fa1af8906131fcbd5a6983be8e4166c
Egor Doronchenko <massda@yandex.ru>
2023-07-02 22:55:54 +0300

    А практически ничего не изменилось

--- Examples/Examples007_dots/Program.cs
+++ Examples/Examples007_dots/Program.cs
@@ -4,9 +4,9 @@
 
 int xa = 1;
 int ya = 1;
-int xb = 1;
-int yb = 30;
-int xc = 30, yc = 30;
+int xb = 20;
+int yb = 10;
+int xc = 60, yc = 15;
 Console.SetCursorPosition(xa,ya);
 Console.WriteLine("+");
 Console.SetCursorPosition(xb,yb);
