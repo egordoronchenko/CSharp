a2cfc4e5b7b7a06e2378143aff26263b52023f2d
Egor Doronchenko <massda@yandex.ru>
2023-07-02 14:12:45 +0300

    Тест2

--- Examples/Examples001_HelloWorld/Program.cs
+++ Examples/Examples001_HelloWorld/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 // Закоментим для теста
-
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
