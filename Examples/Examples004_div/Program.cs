dde71e4715aac6c9c20622dafe55e4fc8a252eee
Egor Doronchenko <massda@yandex.ru>
2023-07-02 19:31:10 +0300

    затестирована 4я программа

--- Examples/Examples004_div/Program.cs
+++ Examples/Examples004_div/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double numberA = 12;
+double numberB = 5;
+double div = numberA / numberB;
+Console.WriteLine(div);
