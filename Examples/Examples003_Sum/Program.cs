62157c8aa206158c3a3a83c59c7784f4a48f4a3a
Egor Doronchenko <massda@yandex.ru>
2023-07-02 19:45:20 +0300

    МодифиуцируемExamples003

--- Examples/Examples003_Sum/Program.cs
+++ Examples/Examples003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 3;
-int numberB = 5;
+﻿int numberA = new Random().Next(1,10);//цифры от 1 до 9
+int numberB =new Random().Next(1,10);
 int sum = numberA + numberB;
+Console.WriteLine(numberA);
+Console.WriteLine(numberB);
 Console.WriteLine(sum);
